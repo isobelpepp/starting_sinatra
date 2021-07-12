bd808840a770e7987f288e138e2778deaa159146
jamescondon <jecondon66@icloud.com>
2021-07-12 18:22:10 +0100

    Separation of Concerns: cat in view, message in app

--- app.rb
+++ app.rb
@@ -1,20 +1,6 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
 
-get '/' do
-  "hello!"
-end
-
-get '/secret' do
-  "This is a very very secret message"
-end
-
-get '/name' do
-  "James"
-end
-
 get '/cat' do
-  # "<div>
-    "<img src='https://i.imgur.com/jFaSxym.png'>"
-  # </div>"
+    erb(:index)
 end
