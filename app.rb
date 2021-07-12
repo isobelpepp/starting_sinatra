7b57a8d00552beb4046a1a467999793777fc4bc4
Isobel <isobelpeppiatt@yahoo.co.uk>
2021-07-12 19:28:53 +0100

    Add random name for cat on loading

--- app.rb
+++ app.rb
@@ -1,6 +1,18 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
 
-get '/cat' do
+get '/' do
+    "hello!"
+  end
+  
+  get '/secret' do
+    "This is a very very secret message"
+  end
+  
+  get '/name' do
+    "James"
+  end
+  
+  get '/cat' do
     erb(:index)
-end
+  end
